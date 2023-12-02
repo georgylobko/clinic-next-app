a9701834c460bde40024a0cf8e9750cd90d8c997
Georgii Lobko <georgii.lobko@blacklane.com>
2023-12-02 21:18:25 +0100

    implemented update and delete for doctors table

--- app/doctors/[doctorId]/page.tsx
+++ app/doctors/[doctorId]/page.tsx
@@ -9,61 +9,64 @@ async function create(formData: FormData) {
 	const office_number = formData.get('office_number') as string;
 	const phone = formData.get('phone') as string;
 
-	try {
-		await sql`
+	await sql`
     INSERT INTO doctors (first_name, last_name, office_number, phone)
     VALUES (${first_name}, ${last_name}, ${office_number}, ${phone})
-  `
+  `.catch((error) => {
+		console.error('Failed to create doctor')
+	})
 
-		revalidatePath('/doctors')
-		redirect('/doctors')
-		return { message: `Added a doctor` }
-	} catch (e) {
-		console.log(e)
-		return { message: 'Failed to create doctor' }
-	}
+	revalidatePath('/doctors')
+	redirect('/doctors')
 }
 
 async function update(formData: FormData) {
 	'use server';
+	const id = formData.get('id') as string;
 	const first_name = formData.get('first_name') as string;
 	const last_name = formData.get('last_name') as string;
 	const office_number = formData.get('office_number') as string;
 	const phone = formData.get('phone') as string;
 
-	console.log('update');
+	await sql`
+    UPDATE doctors
+    SET first_name = ${first_name}, last_name = ${last_name}, office_number = ${office_number}, phone = ${phone}
+    WHERE id = ${id}
+  `.catch((error) => {
+		console.error('Failed to create doctor')
+	})
 
-	// try {
-	// 	await sql`
-  //   INSERT INTO doctors (first_name, last_name, office_number, phone)
-  //   VALUES (${first_name}, ${last_name}, ${office_number}, ${phone})
-  // `
-	//
-	// 	redirect('/doctors')
-	// 	return { message: `Added a doctor` }
-	// } catch (e) {
-	// 	return { message: 'Failed to create doctor' }
-	// }
+	revalidatePath(`/doctors/${id}`)
+	revalidatePath('/doctors')
+	redirect('/doctors')
 }
 
 export default async function Page({ params }: { params: { doctorId: string } }) {
+	let data = null
+
+	if (params.doctorId !== 'new') {
+		const result = await sql`SELECT * FROM doctors WHERE id = ${params.doctorId}`
+		data = result.rows[0]
+	}
+
 	return (
 			<form className="max-w-sm mx-auto" action={params.doctorId === 'new' ? create : update}>
+				<input type="hidden" name="id" value={data?.id} />
 				<div className="mb-5">
 					<label htmlFor="first_name" className="block mb-2 text-sm font-medium text-white-900">Имя</label>
-					<input type="text" name="first_name" id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+					<input type="text" name="first_name" id="first_name" defaultValue={data?.first_name} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
 				</div>
 				<div className="mb-5">
 					<label htmlFor="last_name" className="block mb-2 text-sm font-medium text-white-900">Фамилия</label>
-					<input type="text" name="last_name" id="last_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+					<input type="text" name="last_name" id="last_name" defaultValue={data?.last_name} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
 				</div>
 				<div className="mb-5">
 					<label htmlFor="office_number" className="block mb-2 text-sm font-medium text-white-900">Номер кабинета</label>
-					<input type="text" name="office_number" id="office_number" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+					<input type="text" name="office_number" id="office_number" defaultValue={data?.office_number} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
 				</div>
 				<div className="mb-5">
 					<label htmlFor="phone" className="block mb-2 text-sm font-medium text-white-900">Телефон</label>
-					<input type="text" name="phone" id="phone" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+					<input type="text" name="phone" id="phone" defaultValue={data?.phone} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
 				</div>
 				<button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-white-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Submit</button>
 			</form>
--- app/doctors/page.tsx
+++ app/doctors/page.tsx
@@ -2,6 +2,20 @@ import { sql } from '@vercel/postgres'
 import { revalidatePath } from 'next/cache'
 import Link from "next/link";
 
+async function deleteDoctor(formData: FormData) {
+	'use server';
+	const id = formData.get('id') as string;
+
+	await sql`
+    DELETE FROM doctors
+    WHERE id = ${id}
+  `.catch((error) => {
+		console.error('Failed to delete doctor')
+	})
+
+	revalidatePath('/doctors')
+}
+
 export default async function Page() {
 	const data = await sql`SELECT * FROM doctors`
 	const { rows: doctors } = data
@@ -12,7 +26,7 @@ export default async function Page() {
 				<button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-white-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Добавить врача</button>
 			</Link>
 
-			<table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+			<table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 mt-4">
 				<thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
 				<tr>
 					<th scope="col" className="px-6 py-3">
@@ -26,6 +40,9 @@ export default async function Page() {
 					</th>
 					<th scope="col" className="px-6 py-3">
 						Телефон
+					</th>
+					<th scope="col" className="px-6 py-3">
+						Действия
 					</th>
 				</tr>
 				</thead>
@@ -43,6 +60,22 @@ export default async function Page() {
 							</td>
 							<td className="px-6 py-4">
 								{doctor.phone}
+							</td>
+							<td className="px-6 py-4 flex justify-left">
+								<Link href={`/doctors/${doctor.id}`}>
+									<svg className="w-6 h-6 text-gray-800 dark:text-white mr-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
+										<path d="M12.687 14.408a3.01 3.01 0 0 1-1.533.821l-3.566.713a3 3 0 0 1-3.53-3.53l.713-3.566a3.01 3.01 0 0 1 .821-1.533L10.905 2H2.167A2.169 2.169 0 0 0 0 4.167v11.666A2.169 2.169 0 0 0 2.167 18h11.666A2.169 2.169 0 0 0 16 15.833V11.1l-3.313 3.308Zm5.53-9.065.546-.546a2.518 2.518 0 0 0 0-3.56 2.576 2.576 0 0 0-3.559 0l-.547.547 3.56 3.56Z"/>
+										<path d="M13.243 3.2 7.359 9.081a.5.5 0 0 0-.136.256L6.51 12.9a.5.5 0 0 0 .59.59l3.566-.713a.5.5 0 0 0 .255-.136L16.8 6.757 13.243 3.2Z"/>
+									</svg>
+								</Link>
+								<form action={deleteDoctor}>
+									<input type="hidden" name="id" value={doctor.id} />
+									<button type="submit">
+										<svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
+											<path d="M17 4h-4V2a2 2 0 0 0-2-2H7a2 2 0 0 0-2 2v2H1a1 1 0 0 0 0 2h1v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V6h1a1 1 0 1 0 0-2ZM7 2h4v2H7V2Zm1 14a1 1 0 1 1-2 0V8a1 1 0 0 1 2 0v8Zm4 0a1 1 0 0 1-2 0V8a1 1 0 0 1 2 0v8Z"/>
+										</svg>
+									</button>
+								</form>
 							</td>
 						</tr>
 					))}
