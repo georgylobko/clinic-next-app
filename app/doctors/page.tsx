190f390e0ad72688bdf6e8ee400488d9f7602bf2
Georgii Lobko <georgii.lobko@blacklane.com>
2024-01-22 17:15:23 +0100

    small fix

--- app/doctors/page.tsx
+++ app/doctors/page.tsx
@@ -125,7 +125,7 @@ export default async function Page(props: { searchParams: { first_name?: string;
 							<input type="text" id="first_name" name="first_name" defaultValue={first_name} className="mt-2 block w-full rounded-md border border-gray-200 px-2 py-2 shadow-sm outline-none focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50" />
 						</div>
 						<div className="flex flex-col">
-							<label htmlFor="last_name" className="text-stone-600 text-sm font-medium">Имя</label>
+							<label htmlFor="last_name" className="text-stone-600 text-sm font-medium">Фамилия</label>
 							<input type="text" id="last_name" name="last_name" defaultValue={last_name} className="mt-2 block w-full rounded-md border border-gray-200 px-2 py-2 shadow-sm outline-none focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50" />
 						</div>
 					</div>
