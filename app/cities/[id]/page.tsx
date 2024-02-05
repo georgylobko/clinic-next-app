89568d55cb73f7c0eeed52a176ae3fb8224b19b2
Georgii Lobko <georgii.lobko@blacklane.com>
2024-02-05 23:03:54 +0100

    created cities db entity and UI

---
+++ app/cities/[id]/page.tsx
@@ -0,0 +1,56 @@
+import { sql } from "@vercel/postgres";
+import { redirect } from 'next/navigation'
+import { revalidatePath } from 'next/cache'
+
+async function create(formData: FormData) {
+	'use server';
+	const name = formData.get('name') as string;
+
+	await sql`
+    INSERT INTO cities (name)
+    VALUES (${name})
+  `.catch((error) => {
+		console.error('Failed to create specialization')
+	})
+
+	revalidatePath('/cities')
+	redirect('/cities')
+}
+
+async function update(formData: FormData) {
+	'use server';
+	const id = formData.get('id') as string;
+	const name = formData.get('name') as string;
+
+	await sql`
+    UPDATE cities
+    SET name = ${name}
+    WHERE id = ${id}
+  `.catch((error) => {
+		console.error('Failed to update city')
+	})
+
+	revalidatePath(`/cities/${id}`)
+	revalidatePath('/cities')
+	redirect('/cities')
+}
+
+export default async function Page({ params }: { params: { id: string } }) {
+	let data = null
+
+	if (params.id !== 'new') {
+		const result = await sql`SELECT * FROM cities WHERE id = ${params.id}`
+		data = result.rows[0]
+	}
+
+	return (
+			<form className="max-w-sm mx-auto" action={params.id === 'new' ? create : update}>
+				<input type="hidden" name="id" value={data?.id} />
+				<div className="mb-5">
+					<label htmlFor="name" className="block mb-2 text-sm font-medium text-white-900">Название города</label>
+					<input type="text" name="name" id="name" defaultValue={data?.name} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+				</div>
+				<button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-white-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Submit</button>
+			</form>
+	)
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -74,6 +74,16 @@ export default function RootLayout({
                   <span className="ms-3">Страны</span>
                 </Link>
               </li>
+              <li>
+                <Link href="/cities" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                  <span className="ms-3">Города</span>
+                </Link>
+              </li>
+              <li>
+                <Link href="/streets" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                  <span className="ms-3">Улицы</span>
+                </Link>
+              </li>
             </ul>
           </div>
         </aside>
